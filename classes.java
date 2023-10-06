bc0c574139fd5f80764c4adf34d06afb4f7ee536
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2023-10-07 00:53:28 +0530

    new file

---
+++ classes.java
@@ -0,0 +1,19 @@
+class classes {
+    classes() {
+        System.out.println("class classes called");
+    }
+
+    classes(int a) {
+        System.out.println("single parameterised called");
+    }
+    public static void main(String[] args) {
+        classes ob = new classes();
+    }
+
+
+}
+
+
+class Innerclasses {
+    
+}
